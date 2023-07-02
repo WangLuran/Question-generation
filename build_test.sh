39c1e69cd6748e1a07c5d3248d818996e2beee27
WangLuran <94464527+WangLuran@users.noreply.github.com>
2023-07-02 01:57:00 +0100

    Update build_test.sh

--- build_test.sh
+++ build_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ~/.bashrc
 conda activate Jtest
-python ChatGPT_QG.py --api_key="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" --save_path="chatgpt-qg.json"
+python ChatGPT_QG.py --api_key=""--save_path="chatgpt-qg.json"
 
 conda deactivate
